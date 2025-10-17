229a9725ebf3446c728d77a36586a3a0f7e6f863
Deuslleyw <Deuslleyw@hotmail.com>
2025-10-17 02:41:08 -0300

    firstCommit

---
+++ src/main/java/com/deusleydev/api_desafio_meli/ApiDesafioMeliApplication.java
@@ -0,0 +1,13 @@
+package com.deusleydev.api_desafio_meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioMeliApplication.class, args);
+	}
+
+}
